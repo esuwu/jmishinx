4eecd634b906ba2f559d1c93ca23b3f438c2ffb3
UsatiyNyan <kirillarionov123456@gmail.com>
2020-09-29 23:56:41 +0300

    http started

---
+++ multithread/lib/http/include/http_response.h
@@ -0,0 +1,14 @@
+//
+// Created by kira on 29.09.2020.
+//
+
+#ifndef CPP_MULTITHREAD_LIB_HTTP_INCLUDE_HTTP_RESPONSE_H_
+#define CPP_MULTITHREAD_LIB_HTTP_INCLUDE_HTTP_RESPONSE_H_
+
+namespace http {
+class HttpResponse {
+
+};
+}  // namespace http
+
+#endif //CPP_MULTITHREAD_LIB_HTTP_INCLUDE_HTTP_RESPONSE_H_
---
+++ multithread/lib/http/src/http_request.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kira on 29.09.2020.
+//
+
+#include "http_request.h"
